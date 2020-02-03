6b0f741ebdda877888bd83dc5c14528f423690b3
afouch <afouch1@ahoo.com>
2020-02-02 21:22:51 -0500

    first ocmmit

---
+++ Result.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Dynamic;
+using System.Xml.Serialization;
+
+namespace ResultType
+{
+    public struct Result<T>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a positive return value
+        /// </summary>
+        /// <param name="value"></param>
+        internal Result(T value)
+        {
+            _value = value;
+            IsOk = true;
+            ErrorMessage = $"{value} of type {value.GetType()} has no Error";
+        }
+
+        /// <summary>
+        /// Constructs a negative return value
+        /// </summary>
+        /// <param name="error"></param>
+        internal Result(string error)
+        {
+            _value = default;
+            IsOk = false;
+            ErrorMessage = error;
+        }
+        
+        #endregion
+
+
+        #region Methods
+
+        /// <summary>
+        /// Throw an InvalidOperationException if result is an error
+        /// </summary>
+        /// <param name="errorMessage">Error message for the InvalidOperationException</param>
+        /// <returns></returns>
+        public T Expect(string errorMessage) => 
+            IsOk ? _value : throw new InvalidOperationException(errorMessage);
+
+        /// <summary>
+        /// Returns the specified value if the result is an error
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Result<T> ValueOr(T value) => IsOk ? _value : value;
+
+        public static implicit operator Result<T>(T value) => Result.Ok(value);
+        
+        #endregion
+        
+        #region Members
+        
+        private T _value;
+
+        public T Value => 
+            IsOk ? _value : throw new InvalidOperationException("Attempted to get value unsuccessfully");
+
+        /// <summary>
+        /// Returns the default value if hte result is an error
+        /// </summary>
+        public T ValueOrDefault => IsOk ? _value : default;
+
+        public readonly string ErrorMessage;
+
+        public readonly bool IsOk;
+        
+        #endregion
+    }
+
+    public static class Result
+    {
+        /// <summary>
+        /// A positive result
+        /// </summary>
+        /// <param name="value"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Result<T> Ok<T>(T value) => new Result<T>(value);
+        
+        /// <summary>
+        /// A negative result
+        /// </summary>
+        /// <param name="error"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Result<T> Error<T>(string error) => new Result<T>(error);
+
+        /// <summary>
+        /// Performs a function if the result is not an error
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static Result<T> Then<T, A>(this Result<A> res, Func<A, Result<T>> predicate)
+        {
+            if (res.IsOk) return predicate(res.Value);
+            else return Error<T>(res.ErrorMessage);
+        }
+    }
+}
---
+++ ResultTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ResultType;
+using static ResultType.Result;
+
+namespace ResultTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = ParseString("45u")
+                .Then(n => Ok(new DateTime(2020, 4, n)))
+                .Then(d => Ok(d.ToString()));
+
+            var dateRes = CreateDate(2020, 14, 20);
+            
+            if (dateRes.IsOk)
+                Console.WriteLine(dateRes.Value);
+            else
+                Console.WriteLine(dateRes.ErrorMessage);
+        }
+
+        static Result<DateTime> CreateDate(int year, int month, int day)
+        {
+            if (year > 2020 || month > 12 || month < 1 || day < 1 || day > 31)
+                return Error<DateTime>("Wrong Date");
+            return new DateTime(year, month, day);
+        }
+        
+        static Result<int> Multiply(int a, int b)
+        {
+            if (a < 0) return Error<int>(a + " is less than zero");
+            if (b < 0) return Error<int>(b + " is less than zero");
+            else return a * b;
+        }
+
+        static Result<int> ParseString(string text)
+        {
+            return int.TryParse(text, out int num) ?
+                num : 
+                Error<int>($"{text} is not a number");
+        }
+    }
+}
